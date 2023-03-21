628ab4e9bb7970962e5a89d5e8072bcab806de26
ozzlems <ozlemsencoruh@gmail.com>
2023-03-22 01:25:49 +0300

    targetted class and id names

---
+++ index.js
@@ -0,0 +1,10 @@
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+
+
+let username = id("username"),
+email = id("email"),
+ password =id("password"),
+ errorMsg = classes("error"),
+ successIcon = classes("success-icon"),
+ failureIcon = classes("failure-icon");
